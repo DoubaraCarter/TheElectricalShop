7da3d627ad824ba2c1af61421ffc1dfb6a8d9c4f
Asus <Doubaracarter@gmail.com>
2025-01-30 10:20:02 +0100

    Completed the landing page

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 window.addEventListener('scroll', function () {
     const navbar = document.getElementById('navbar');
+    const navlinks = document.getElementById('navlinks');
+    const navbtn = document.getElementById('nav-btn')
     if (window.scrollY > 0) {
       navbar.classList.add('bg-white', 'shadow-md');
+      navlinks.classList.add('text-black')
+      navbtn.classList.add('text-black')
     } else {
       navbar.classList.remove('bg-white', 'shadow-md');
+      navlinks.classList.remove('text-black')
+      navbtn.classList.remove('text-black')
     }
   });
